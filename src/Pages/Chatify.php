809eecc6efc27b5c73aa68631245bbb591cc79b2
t0nka <stefan.hoheisel@gmail.com>
2025-09-22 10:25:27 +0200

    Change view property to instance variable in Chatify

--- src/Pages/Chatify.php
+++ src/Pages/Chatify.php
@@ -7,7 +7,7 @@ use Filament\Pages\Page;
 class Chatify extends Page
 {
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-chat-bubble-oval-left';
-    protected static string $view = 'filament-chatify-integration::pages.chatify';
+    protected string $view = 'filament-chatify-integration::pages.chatify';
     protected static ?string $navigationLabel = "Chatify";
     protected static ?string $title = "Chatify";
     protected static ?string $slug = "chatify";
