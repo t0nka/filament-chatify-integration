abb7da98be09914fc41289f7fde40d1cf0aa9307
t0nka <stefan.hoheisel@gmail.com>
2025-09-22 10:24:42 +0200

    Update navigationIcon type to support BackedEnum

--- src/Pages/Chatify.php
+++ src/Pages/Chatify.php
@@ -6,7 +6,7 @@ use Filament\Pages\Page;
 
 class Chatify extends Page
 {
-    protected static ?string $navigationIcon = 'heroicon-o-chat-bubble-oval-left';
+    protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-chat-bubble-oval-left';
     protected static string $view = 'filament-chatify-integration::pages.chatify';
     protected static ?string $navigationLabel = "Chatify";
     protected static ?string $title = "Chatify";
